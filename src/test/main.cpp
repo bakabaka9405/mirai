d2ee22ea994786fd21ec2c58d7323256e851a04c
bakabaka9405 <2677423642@qq.com>
2024-01-28 20:46:30 +0800

    简要实现了二分函数的模板

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,27 +1,22 @@
-#include <ios>
-#include <mirai/graph/tree_decomposition.hpp>
-#include <mirai/graph/iterate.hpp>
 #include <mirai/util/pipeline.hpp>
 #include <mirai/util/io.hpp>
 #include <mirai/math/kahan.hpp>
+#include <mirai/misc/binary_search.hpp>
 using namespace mirai;
 ll n, m;
 array<ll, 100001> a, b;
 int main() {
 	input(n, m);
 	for (auto i : views::iota(1ll, n + 1)) cin >> a[i] >> b[i];
-	if (m >= std::accumulate(a.begin() + 1, a.begin() + 1 + n, 0)) {
+	if (m >= std::accumulate(a.begin() + 1, a.begin() + 1 + n, 0ll)) {
 		cout << -1 << endl;
 		return 0;
 	}
-	double l = 0, r = 1e10, mid;
-	for (auto _ : views::iota(200ll)) {
-		mid = (l + r) / 2;
+	double ans = float_bsearch_by_times<dextral>(0, 1e10, 200, [&](double mid) {
 		kahan sum;
 		for (auto i : views::iota(1ll, n + 1) | transform([&](ll x) { return a[x] * mid - b[x]; }) | filter(is_positive)) sum += i;
-		if(*sum<=mid*m)l=mid;
-		else r=mid;
-	}
-	cout<<std::fixed<<setprecision(10)<<l<<endl;
+		return (*sum <= mid * m);
+	});
+	cout << std::fixed << setprecision(10) << ans << endl;
 	return 0;
 }
