bdfb7e247f82a77774fa5d16d1ce84b6184cfca1
bakabaka9405 <2677423642@qq.com>
2024-02-19 12:44:29 +0800

    优化 persistent_array 性能

--- include/mirai/ds/persistent_array.hpp
+++ include/mirai/ds/persistent_array.hpp
@@ -6,8 +6,12 @@ struct segment_tree_node {
 	T val;
 	segment_tree_node* left;
 	segment_tree_node* right;
-	explicit segment_tree_node(T val, segment_tree_node* left = nullptr, segment_tree_node* right = nullptr) mr_noexcept
+	explicit segment_tree_node(const T& val, segment_tree_node* left = nullptr, segment_tree_node* right = nullptr) mr_noexcept
 		: val(val),
+		  left(left),
+		  right(right) {}
+	explicit segment_tree_node(T&& val, segment_tree_node* left = nullptr, segment_tree_node* right = nullptr) mr_noexcept
+		: val(std::move(val)),
 		  left(left),
 		  right(right) {}
 };
@@ -16,17 +20,19 @@ class persistent_array {
 private:
 	Allocator alloc;
 	Node* root;
-	Node* construct(T val, Node* left = nullptr, Node* right = nullptr) {
+	Node* construct(auto&& val, Node* left = nullptr, Node* right = nullptr) mr_noexcept {
 		if constexpr (requires { alloc.new_object; }) {
-			return alloc.template new_object<Node>(val, left, right);
+			return alloc.template new_object<Node>(std::forward<T>(val), left, right);
 		}
 		else {
 			Node* x = alloc.allocate(1);
-			alloc.construct(x, val, left, right);
+			alloc.construct(x, std::forward<T>(val), left, right);
 			return x;
 		}
 	}
-	const T& get(Node* x, ull pos, ull L, ull R) const mr_noexcept {
+	const T& visit(ull pos) const mr_noexcept {
+		Node* x = root;
+		ull L = 0, R = size;
 		while (L != R) {
 			MR_ASSUME(x != nullptr);
 			ull M = (L + R) / 2;
@@ -41,33 +47,35 @@ private:
 		}
 		return x->val;
 	}
-	pair<Node*, T&> get(Node* x, ull pos, ull L, ull R) mr_noexcept {
-		MR_ASSUME(x != nullptr);
-		if (L == R) {
-			auto obj = construct(x->val);
-			return { obj, obj->val };
+	T& visit(ull pos) mr_noexcept {
+		root = construct(root->val, root->left, root->right);
+		Node* x = root;
+		ull L = 0, R = size;
+		while (L != R) {
+			MR_ASSUME(x != nullptr);
+			ull M = (L + R) / 2;
+			if (pos <= M) {
+				x = x->left = construct(x->left->val, x->left->left, x->left->right);
+				R = M;
+			}
+			else {
+				x = x->right = construct(x->right->val, x->right->left, x->right->right);
+				L = M + 1;
+			}
 		}
-		ull M = (L + R) / 2;
-		if (pos <= M) {
-			auto [left, ref] = get(x->left, pos, L, M);
-			return { construct(x->val, left, x->right), ref };
-		}
-		else {
-			auto [right, ref] = get(x->right, pos, M + 1, R);
-			return { construct(x->val, x->left, right), ref };
-		}
+		return x->val;
 	}
-	Node* set(Node* x, ull pos, T val, ull L, ull R) mr_noexcept {
-		if (L == R) return construct(val);
+	Node* set(Node* x, ull pos, auto&& val, ull L, ull R) mr_noexcept {
+		if (L == R) return construct(std::forward<T>(val));
 		ull M = (L + R) / 2;
 		MR_ASSUME(x != nullptr);
 		if (pos <= M)
-			return construct(x->val, set(x->left, pos, val, L, M), x->right);
+			return construct(x->val, set(x->left, pos, std::forward<T>(val), L, M), x->right);
 		else
-			return construct(x->val, x->left, set(x->right, pos, val, M + 1, R));
+			return construct(x->val, x->left, set(x->right, pos, std::forward<T>(val), M + 1, R));
 	}
 	template <size_t N>
-	Node* set(T (&arr)[N], ull L, ull R) {
+	Node* build(T (&arr)[N], ull L, ull R) {
 		if (L == R) {
 			if (L < N)
 				return construct(arr[L]);
@@ -75,19 +83,22 @@ private:
 				return construct(T{}); // default value
 		}
 		ull M = (L + R) / 2;
-		return construct(0, set(arr, L, M), set(arr, M + 1, R));
+		return construct(0, build(arr, L, M), build(arr, M + 1, R));
 	}
 	explicit persistent_array(Node* root, Allocator alloc = {})
 		: alloc(alloc), root(root) {}
 
 public:
 	persistent_array() mr_noexcept
-		: alloc{}, root(nullptr) {}
+		: alloc{},
+		  root(nullptr) {}
 	explicit persistent_array(std::pmr::monotonic_buffer_resource* pool) mr_noexcept
-		: alloc{ pool }, root(nullptr) {}
+		: alloc{ pool },
+		  root(nullptr) {}
 	template <size_t N>
 	persistent_array(T (&val)[N], std::pmr::monotonic_buffer_resource* pool) mr_noexcept
-		: alloc(pool), root(set(val, 0, size)) {}
+		: alloc(pool),
+		  root(build(val, 0, size)) {}
 	persistent_array(const persistent_array& x)
 		: alloc{ x.alloc }, root(x.root) {}
 	persistent_array(persistent_array&& x) noexcept
@@ -102,16 +113,17 @@ public:
 	}
 	template <size_t N>
 	persistent_array& operator=(T (&val)[N]) mr_noexcept {
-		root = set(val, 0, size);
+		root = build(val, 0, size);
 		return *this;
 	}
+	persistent_array set_copy(ull pos, auto&& val) mr_noexcept {
+		return persistent_array(set(root, pos, std::forward<T>(val), 0, size), alloc);
+	}
 	const T& operator[](ull pos) const mr_noexcept {
-		return get(root, pos, 0, size);
+		return visit(pos);
 	}
 	T& operator[](ull pos) mr_noexcept {
-		auto&& [new_root, ref] = get(root, pos, 0, size);
-		root = new_root;
-		return ref;
+		return visit(pos);
 	}
 };
 MR_NAMESPACE_END
