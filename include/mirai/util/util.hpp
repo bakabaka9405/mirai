5731669dcb6a1799abb5c7d9015156e4b4f27bf0
bakabaka9405 <2677423642@qq.com>
2024-05-24 16:04:09 +0800

    加入 make_mdvector

--- include/mirai/util/util.hpp
+++ include/mirai/util/util.hpp
@@ -7,4 +7,16 @@ inline void to_max(auto& a, auto&& b) mr_noexcept {
 inline void to_min(auto& a, auto&& b) mr_noexcept {
 	a = std::min(a, b);
 }
+
+template <typename T = ll>
+constexpr auto make_mdvector(auto&& n, auto&&... m) {
+	if constexpr (sizeof...(m) == 0) {
+		return vector<T>(n);
+	}
+	else {
+		using Y = decltype(make_mdvector<T>(m...));
+		return vector<Y>(n, make_mdvector<T>(m...));
+	}
+}
+
 MR_NAMESPACE_END
