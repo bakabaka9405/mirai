7f812469cb0ae620dc75112c25115c2fa5d9a537
bakabaka9405 <2677423642@qq.com>
2024-09-29 19:38:20 +0800

    删除部分没用的pipeline函数

--- include/mirai/util/pipeline/pipeline.hpp
+++ include/mirai/util/pipeline/pipeline.hpp
@@ -11,17 +11,6 @@ constexpr auto from(iter_t&& it) {
 	};
 	return from_wrapper{ std::forward<iter_t>(it) };
 }
-
-template <range range>
-inline constexpr auto in_array(range&& arr, ull l, ull r) {
-	struct in_array_wrapper {
-		range arr;
-		ull l, r;
-		inline constexpr auto begin() const mr_noexcept { return std::next(mr_begin(arr), l); }
-		inline constexpr auto end() const mr_noexcept { return std::next(mr_begin(arr), r + 1); }
-	};
-	return in_array_wrapper{ std::forward<range>(arr), l, r };
-}
 class __cycle_helper {
 private:
 	template <range _range>
@@ -163,50 +152,6 @@ MR_NODISCARD inline constexpr auto take(size_t n) {
 	return __take_helper{ n };
 }
 
-template <range _range>
-MR_NODISCARD inline constexpr auto in_column(_range&& r, ull col) {
-	struct in_column_wrapper {
-		_range _r;
-		const ull _col;
-		struct iterator {
-			using iter_t = decltype(mr_begin(_r));
-			_range& _rg;
-			iter_t _it;
-			const ull _col;
-			inline bool operator!=(const std::default_sentinel_t&) const mr_noexcept {
-				return _it != mr_end(_rg);
-			}
-			inline decltype(auto) operator++() mr_noexcept {
-				++_it;
-			}
-			inline auto operator++(int) mr_noexcept->iterator {
-				return iterator{ _rg, _it++, _col };
-			}
-			inline auto operator*() const mr_noexcept {
-				return (*_it)[_col];
-			}
-		};
-		inline auto begin() const mr_noexcept {
-			return iterator{ _r, mr_begin(_r), _col };
-		}
-		inline constexpr auto end() const mr_noexcept {
-			return std::default_sentinel;
-		}
-	};
-	return in_column_wrapper{ std::forward<_range>(r), col };
-}
-
-struct __in_column_helper {
-	const ull col;
-	template <range _range>
-	MR_NODISCARD friend inline auto operator|(_range&& lhs, __in_column_helper self) {
-		return in_column(std::forward<_range>(lhs), self.col);
-	}
-};
-
-MR_NODISCARD inline constexpr auto in_column(ull col) {
-	return __in_column_helper{ col };
-}
 
 template <range _range>
 inline auto skip(_range&& r, size_t n) {
